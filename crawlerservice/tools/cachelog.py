9c1dc933787e66327a10e5b6dc48cfff9bc0e220
Li Ding <lidingpku@gmail.com>
2016-06-02 00:25:35 -0700

    bufix cachelog

--- crawlerservice/tools/cachelog.py
+++ crawlerservice/tools/cachelog.py
@@ -29,15 +29,21 @@ def get_logger(batch_id, today_str):
                 pass
         return log.init(the_batch_id, absfname, level=logging.INFO, size=100*1024*1024)
 
+    need_update_loggers = False
     if not hasattr(get_logger, '_ipaddr'):
         setattr(get_logger, '_ipaddr', ip.get_local_ip_address())
+        need_update_loggers = True
 
     if not hasattr(get_logger, '_today_str') or get_logger._today_str != today_str:
         setattr(get_logger, '_today_str', today_str)
-        
-        the_logger = filename_in_logger_out(batch_id)
-        loggers = getattr(get_logger, '_logger', {})
-        loggers[batch_id] = the_logger
+        need_update_loggers = True
+
+    loggers = getattr(get_logger, '_logger', {})
+    if batch_id not in loggers:
+        need_update_loggers = True
+
+    if need_update_loggers:
+        loggers[batch_id] =  filename_in_logger_out(batch_id)
         setattr(get_logger, '_logger', loggers)
 
     return get_logger._logger[batch_id]
