cb0c577ab19c7da534fc1f2f1edc79cf7c78c63a
yuande <miraclecome@gmail.com>
2016-06-03 22:48:52 +0800

    add schedule

--- awscrawler/schedule.py
+++ awscrawler/schedule.py
@@ -1,114 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+from __future__ import print_function, division
+from math import ceil
+
 import time
-import boto3
-import re
-import logging
-import traceback
 
-AWS_ACCESS_KEY_ID = 'AKIAIFM563SKK5KM6F5A'
-AWS_SECRET_ACCESS_KEY = 'BMOgdoiwN4q+Mio7csBe4riZ7DPqUx0gIQrtrhYZ+Y8/Y59ttsp'
-sg_names = 'worker'  # sg-048eab63 launch-wizard-1
-REGION_NAME = 'ap-northeast-1'
-INSTANCE_TYPE = 't2.micro'
-IMA_ID = "IMA"
-
-
-class EC2manager(object):
-
-    def get_connected(self, ):
-        self.conn = boto3.resource('ec2', region_name='ap-northeast-1', aws_access_key_id='AKIAIFM563SKK5KM6F5A',
-                                   aws_secret_access_key='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')
-
-    def __init__(self):
-        self.conn = None
-        self.instances = None
-        self.ids = []
-
-    def create_instances(self, ImageId='ami-d7d4c5b9', MinCount=1, MaxCount=1, KeyName='kg-proxy-tokyo', InstanceType='t2.micro'):
-        ins = self.conn.create_instances(
-            ImageId=ImageId, MinCount=MinCount, MaxCount=MaxCount, KeyName=KeyName, InstanceType=InstanceType)
-
-        ins_id = re.compile("'(.*)'").findall(str(ins[0]))[0]
-
-    def filter(self, status):
-        instances = self.conn.instances.filter(
-            Filters=[{'Name': 'instance-state-name', 'Values': status}])
-        ins_id = []
-        for instance in instances:
-            ins_id.append(instance.id)
-        return ins_id
-
-    def get_instances(self):
-        # have not check the stopping
-        print 'Running instances :   '
-        print self.filter(['running'])
-
-        print '\nStopped instances: '
-        print self.filter(['stopped'])
-
-        ing=self.filter(['stopping','pending','shutting-down'])
-        if ing:
-        	print   'processing instances:'
-        	print ing
-    def get_ids(self):
-
-        return self.ids
-
-    def start_all(self):
-        id_list = self.filter(['running', 'stopped', 'stopping'])
-        if(id_list):
-            self.conn.instances.filter(InstanceIds=id_list).start()
-
-    def stop_all(self):
-        id_list = self.filter(['running'])
-        if(id_list):
-            self.conn.instances.filter(InstanceIds=id_list).stop()
-
-    def terminate_all(self):
-        instances = self.conn.instances.filter(
-            Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'stopped']}])
-        id_list = []
-        for instance in instances:
-            id_list.append(instance.id)
-        if(id_list):
-            self.conn.instances.filter(InstanceIds=id_list).terminate()
-
-    def start(self, ins_id):
-        ins_idlist = []
-        ins_idlist.append(ins_id)
-        self.conn.instances.filter(InstanceIds=ins_idlist).start()
-
-    def stop(self, ins_id):
-        ins_idlist = []
-        ins_idlist.append(ins_id)
-        self.conn.instances.filter(InstanceIds=ins_idlist).stop()
-
-    def create_image(self):
-        self.image = create_image(instance_id, name)
-
+from ec2manager import Ec2Manager
 
 class schedule(object):
 
-    def __init__(self, *args, **kwargs):
-        self.restart_duration = 300
-        self.num = num
-        self.ids = ids
+    def __init__(self, machine_num, *args, **kwargs):
+        self.machine_num = machine_num
+        self.cycle = kwargs.get('cycle', 600)
+        self.group_num = int(ceil(machine_num * 2 / self.cycle)) if machine_num * 2 >= self.cycle else 1
+        self.restart_interval = 0 if machine_num * 2 >= self.cycle else self.cycle / machine_num 
 
-    def control(self):
-        now = time.time()
-        while(1):
-            run_all()
-            time.sleep(self.restart_duration)
-            stop_all()
-            if ():
-                break
-            pass
+        self.ec2manager = Ec2Manager()
 
-a = EC2manager()
-a.get_connected()
+    def run(self):
+        self.ec2manager.create_instances(self.machine_num)
+        time.sleep(30)
 
-
-"""
-a.create_instances()
-"""
-a.get_instances()
-a.start_all()
+        before = time.time()
+        while 1:
+            self.ec2manager.stop_one_and_restart()
+            if self.restart_interval != 0:
+                now = time.time()
+                sleep_interval = before + self.restart_interval - now
+                if sleep_interval > 0:
+                    time.sleep(sleep_interval)
+                before = now
