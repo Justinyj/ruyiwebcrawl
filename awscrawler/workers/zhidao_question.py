5cbb9cb9b550276c49dbdce7f3219275c2f57019
ruyi <120559187@qq.com>
2016-06-07 14:12:10 +0800

    20160607

--- awscrawler/workers/zhidao_question.py
+++ awscrawler/workers/zhidao_question.py
@@ -25,16 +25,7 @@ HEADER = {
     'Cache-Control': 'max-age=0',
     'Upgrade-Insecure-Requests': '1'
 }
-HEADER = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'zh-CN,en-US;q=0.8,en;q=0.6',
-    'Host': 'zhidao.baidu.com',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36',
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1'
-}
+
 
 
 def parse_q_id(content):
