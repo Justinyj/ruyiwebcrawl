54a231968f306c79be41d0102756e2eca27b5cb3
ruyi <120559187@qq.com>
2016-06-07 14:09:01 +0800

    20160607

--- awscrawler/invoker/zhidao.py
+++ awscrawler/invoker/zhidao.py
@@ -6,30 +6,7 @@ from __future__ import print_function, division
 
 from awscrawler import post_job
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 13b6908d965985381765df5cfc06dc475a546bc4
-BATCH_ID = {
-    'question': 'zhidao-question-20160606',
-    'answer': 'zhidao-answer-20160606',
-    'json': 'zhidao-json-20160606',  #
-    'result': 'zhidao-result-20160606'  # 合并后
-}
-<<<<<<< HEAD
-HEADER = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'zh-CN,en-US;q=0.8,en;q=0.6',
-    'Host': 'zhidao.baidu.com',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36',
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1'
-}
-=======
-
->>>>>>> 13b6908d965985381765df5cfc06dc475a546bc4
 def load_urls(fname):
     with open(fname) as fd:
         return [i.strip() for i in fd if i.strip() != '']
