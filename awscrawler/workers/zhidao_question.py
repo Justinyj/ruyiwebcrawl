84bc84b2659675c57dc7a148dbc426b727b913e2
ruyi <120559187@qq.com>
2016-06-07 17:50:17 +0800

    123

--- awscrawler/workers/zhidao_question.py
+++ awscrawler/workers/zhidao_question.py
@@ -33,18 +33,18 @@ def parse_title(content):
         title = m.group(1)
         if ("_百度知道") not in title:
             if ("百度知道-信息提示") == title:
-                return 
-            return 
+                return
+            return
         title = re.sub("_百度知道", "", title)
         return title
-    return 
+    return
 
 
 def parse_q_time(content):
     m = re.search(
         '<em class="accuse-enter">.*\n*</ins>\n*(.*)\n*</span>', content)
     if m is None:
-        return 
+        return
     q_time = m.group(1)
     return q_time
 
@@ -61,7 +61,7 @@ def parse_q_content(content):
             supply = n.group(1)
             q_content = q_content + supply
         return q_content
-    return 
+    return
 
 
 def parse_answer_ids(content):
@@ -73,7 +73,7 @@ def generate_question_json(content):
     q_title = parse_title(content)
     if q_title is None:
         # print('未找到title或者页面不存在')
-        return 
+        return
     q_id = parse_q_id(content)
     q_content = parse_q_content(content)
     q_time = parse_q_time(content)
@@ -110,9 +110,10 @@ def worker(url, parameter, *args, **kwargs):
         flag = m.post(url, ans_content)
 
     distributed = get_distributed_queue(BATCH_ID['answer'])
-    ids=json.loads(question_content)['answers'][:3]
-    for rid in ids:
-        answer_url = get_answer_url(qid, rid)
+    qid = parse_q_id(content)
+    answer_ids = json.loads(question_content)['answers'][:3]
+    for answer_id in answer_ids:
+        answer_url = get_answer_url(qid, answer_id)
         put_url_enqueue(BATCH_ID['answer'], answer_url, distributed)
 
     return flag
