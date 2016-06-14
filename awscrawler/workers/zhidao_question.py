9244a651ca38c2117e260f1e7076e783d6e9bc15
yuande <miraclecome@gmail.com>
2016-06-14 11:34:23 +0800

    refactor zhidao parse

--- awscrawler/workers/zhidao_question.py
+++ awscrawler/workers/zhidao_question.py
@@ -7,81 +7,15 @@ import time
 import os
 import sys
 import re
-import json
 import base64
 import traceback
 import requests
+
 from invoker.zhidao import BATCH_ID, HEADER
-from zhidao_tools import get_zhidao_content, get_answer_url
 from downloader.cache import Cache
+from .zhidao_tools import get_zhidao_content, get_answer_url
+from .zhidao_parser import parse_title, parse_q_time, parse_q_content, parse_answer_ids, generate_question_json
 
-
-def parse_title(content):
-    m = re.search('<title>(.*)</title>', content)
-    if m:
-        title = m.group(1)
-        if u'_百度知道' not in title:
-            if u'百度知道-信息提示' == title:
-                return
-            return
-        title = re.sub(u'_百度知道', u'', title)
-        return title
-    return
-
-
-def parse_q_time(content):
-    m = re.search(
-        '<em class="accuse-enter">.*\n*</ins>\n*(.*)\n*</span>', content)
-    if m is None:
-        return
-    q_time = m.group(1)
-    return q_time
-
-
-def parse_q_content(content):
-    q_content = ''
-    m = re.search('accuse="qContent">(.*?)(</pre>|</div>)', content)
-    n = re.search('accuse="qSupply">(.*?)(</pre>|</div>)', content)
-
-    if m:
-        q_content = m.group(1)
-        q_content = re.sub('<.*?>', '\n', q_content)
-        q_content = q_content.strip()
-    if n:
-        supply = n.group(1)
-        q_content += supply
-
-    if 'word-replace' in q_content:
-        return
-
-    return q_content
-
-
-def parse_answer_ids(content):
-    result = map(int, re.findall('id="answer-(\d+)', content))
-    return result
-
-
-def generate_question_json(q_id, content, answer_ids):
-    q_title = parse_title(content)
-    if q_title is None:
-        # print('未找到title或者页面不存在')
-        return
-    q_content = parse_q_content(content)
-    if q_content is None:
-        return
-
-    q_time = parse_q_time(content)
-    rids = parse_answer_ids(content)
-    item = {
-        'question_id': q_id,
-        'question_title': q_title,
-        'question_detail': q_content,
-        'question_time': q_time,
-        'answers': rids,
-    }
-    answer_ids.extend(rids)
-    return json.dumps(item)
 
 
 def process(url, parameter, manager, *args, **kwargs):
