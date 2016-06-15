8c6130c40b31122fe51ea03eaf7266b1cc94bbab
yuande <miraclecome@gmail.com>
2016-06-15 16:20:16 +0800

    add restful api receiver

---
+++ projects/qastream/receiver/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+from __future__ import print_function, division
+
+import tornado.httpserver
+import tornado.ioloop
+
+from tornado.options import define, options
+from router import urls
+
+
+define('port', default=8100, type=int, help='process port')
+define('process', default=1, type=int, help='process number')
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+
+    http_server = tornado.httpserver.HTTPServer(urls, xheaders=True)
+    http_server.bind(options.port)
+    http_server.start(options.process)
+    tornado.ioloop.IOLoop.current().start()
+
