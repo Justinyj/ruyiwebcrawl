6bec0eb985f3a00e6824b983acf07aabd2815a94
yixuan zhao <120559187@qq.com>
2016-06-15 15:21:35 +0800

    Update zhidao_question.py

--- awscrawler/workers/zhidao_question.py
+++ awscrawler/workers/zhidao_question.py
@@ -16,68 +16,9 @@ from .zhidao_parser import parse_title, parse_q_time, parse_q_content, parse_ans
 
 def process(url, parameter, manager, *args, **kwargs):
     m = re.search(
-<<<<<<< HEAD
-        '<em class="accuse-enter">.*\n*</ins>\n*(.*)\n*</span>', content)
-    if m is None:
-        return
-    q_time = m.group(1)
-    return q_time
-
-
-def parse_q_content(content):
-    q_content = ''
-    m = re.search('accuse="qContent">(.*?)(</pre>|</div>)', content)
-    n = re.search('accuse="qSupply">(.*?)(</pre>|</div>)', content)
-    if m:
-        q_content = m.group(1)
-        q_content = re.sub('<.*?>', '\n', q_content)
-        q_content = q_content.strip()
-    if n:
-        supply = n.group(1)
-        q_content += supply
-    if q_content:
-        return q_content
-    return
-
-
-def parse_answer_ids(content):
-    res=[]
-    pro = re.search('id="answer-(/d+)" class="answer quality-info line">')
-    if pro :
-        res.append(pro.group(1))
-    answers=re.findall('class="bd answer.*" id="answer-(\d+)"',content)
-    res.extend(map(int,answers))
-    return res
-
-
-def generate_question_json(content, answer_ids):
-    q_title = parse_title(content)
-    if q_title is None:
-        # print('未找到title或者页面不存在')
-        return
-    q_id = parse_q_id(content)
-    q_content = parse_q_content(content)
-    if 'word-replace' in q_content:
-        #   setattr(generate_question_json, '_imgcontent_counter', 0)
-        #generate_question_json._imgcontent_counter += 1
-        return
-    q_time = parse_q_time(content)
-    rids = parse_answer_ids(content)
-    item = {
-        'question_id': q_id,
-        'question_title': q_title,
-        'question_detail': q_content,
-        'question_time': q_time,
-        'answers': rids,
-    }
-    answer_ids.extend(rids)
-    return json.dumps(item)
-
-=======
         'http://zhidao.baidu.com/question/(\d+).html', url)
     if not m:
         return False
->>>>>>> 54994b8312ecc11bba8342e40aaa15f5a87dc4c7
 
     q_id = m.group(1)
     method, gap, js, data = parameter.split(':')
