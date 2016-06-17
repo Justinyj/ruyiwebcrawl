8f11509bf4d13ced8d639b6c9948ec34f92d4b47
yuande <miraclecome@gmail.com>
2016-06-17 11:32:07 +0800

    change auto deploy file

--- awscrawler/ec2manager.py
+++ awscrawler/ec2manager.py
@@ -19,7 +19,7 @@ if REGION_NAME == 'us-west-1':
 elif REGION_NAME == 'ap-northeast-1':
     KEYPAIR = 'crawl-tokyo'
     SECURITYGROUPID = 'sg-fcbf0998'
-    AMI_ID = 'ami-8c08e2ed'
+    AMI_ID = 'ami-0359b362'
 
 INSTANCE_TYPE = 't2.nano'
 
