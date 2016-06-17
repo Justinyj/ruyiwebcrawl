2f3c2bd8130e987d01d555a002e3eabe50eb1a24
ruyi <120559187@qq.com>
2016-06-17 21:15:22 +0800

    update

--- projects/mingluji/mingluji/middlewares.py
+++ projects/mingluji/mingluji/middlewares.py
@@ -18,18 +18,3 @@ class MyMiddleWare(object):
             response = scrapy.http.response.html.HtmlResponse(url, encoding='utf-8', body=content)
             return response
         return
-
-'''
-    def process_response(self, request, response, spider):
-        url=response.url
-        if response.status not in [200, 301]:
-            f.write(response.url)
-            return response
-        m=Cache(BATCH_ID)
-        content=m.get(url)
-        if not content:
-        	new_content=response.body
-        	m.post(url,new_content)
-              #print 'I am pppppppppppppppposting to Cache'
-        return response
-'''
