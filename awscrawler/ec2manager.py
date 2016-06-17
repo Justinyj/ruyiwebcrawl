6b07510355eaecb5d0c7a9de756d38cfe6674480
yuande <miraclecome@gmail.com>
2016-06-17 22:17:23 +0800

    change image of ec2

--- awscrawler/ec2manager.py
+++ awscrawler/ec2manager.py
@@ -19,7 +19,7 @@ if REGION_NAME == 'us-west-1':
 elif REGION_NAME == 'ap-northeast-1':
     KEYPAIR = 'crawl-tokyo'
     SECURITYGROUPID = 'sg-fcbf0998'
-    AMI_ID = 'ami-adbd48cc'
+    AMI_ID = 'ami-ffba4f9e'
 
 INSTANCE_TYPE = 't2.nano'
 
