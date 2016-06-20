c545ee9f7f0bb77447c3553c51c7cd9457e93a36
Li Ding <lidingpku@gmail.com>
2016-06-20 10:45:23 +0800

    add general purpose prefetch tool

--- awscrawler/invoker/prefetch_tool.py
+++ awscrawler/invoker/prefetch_tool.py
@@ -16,26 +16,11 @@ import json
 from awscrawler import post_job, delete_distributed_queue
 from schedule import Schedule
 
+VERSION ='v20160620'
+
+
 def getTheFile(filename):
     return os.path.abspath(os.path.dirname(__file__)) +"/"+filename
-
-###########
-# config parameters
-THE_CONFIG = {
-    "batch_id": 'zhidao-search-20160619',
-    "crawl_http_method" : "get",
-    "crawl_gap" : 3,
-    "crawl_use_js_engine" : False,
-    "crawl_timeout" : 10,
-#    "crawl_refresh" : False,
-#    "crawl_result_content_encoding" : 'gb18030',
-#    "crawl_http_headers": {'Host': 'zhidao.baidu.com'},
-    "aws_machine_number" : 20,
-
-    # ln -s /Users/lidingpku/haizhi/project/ruyiwebcrawl/awscrawler/local/
-    "filename_urls" : '/data/awscrawler/local/zhidao_search/urls.20160617.raw.txt'
-#    "debug" : True,
-}
 
 def slack(msg):
     data={
@@ -44,22 +29,28 @@ def slack(msg):
     requests.post("https://hooks.slack.com/services/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", data=json.dumps(data))
 
 def run(config):
-    slack( "run {} debug: {}".format(config["batch_id"], config.get("debug",False)) )
     ts_start = time.time()
+    urls = load_urls(config["filename_urls"])
+
+    slack( u"run {} batch_id: {}, urls: {} debug: {}".format(
+        config["note"],
+        config["batch_id"],
+        len(urls),
+        config.get("debug",False)) )
+
 
     #load urls
     if config.get("debug"):
         print(datetime.datetime.now().isoformat(), 'start load_urls --- dryrun mode')
     else:
         print(datetime.datetime.now().isoformat(), 'start load_urls --- work mode')
-    urls = load_urls(config["filename_urls"])
 
 
     if config.get("debug"):
         print(datetime.datetime.now().isoformat(), 'start post_job')
     tasks = []
     t1 = post_job(
-        config['batch_id'],
+        config["batch_id"],
         config['crawl_http_method'] ,
         config['crawl_gap'],
         config["crawl_use_js_engine"],
@@ -121,5 +112,12 @@ def load_urls(fname):
     return list(ret)
 
 if __name__ == '__main__':
-
-    run(THE_CONFIG)
+    """
+        python invoker/prefetch_tool.py config_dongfangcaifu.json prefetch_index
+    """
+    filename_config = getTheFile("../config_prefetch/"+sys.argv[1])
+    config_index = sys.argv[2]
+    with open(filename_config) as f:
+        config_all = json.load(f)
+        run(config_all[config_index])
+    print ("all done!")
