2776ec87fad889a58ddccb2cbc292b2ab0cc7fa4
yuande <miraclecome@gmail.com>
2016-06-21 20:02:02 +0800

    crawl notice

--- awscrawler/tests/test_dongcaigonggao_url.py
+++ awscrawler/tests/test_dongcaigonggao_url.py
@@ -4,11 +4,14 @@
 
 from __future__ import print_function, division
 
+import os
 import requests
+import urlparse
 import time
 import lxml.html
 from collections import Counter
 
+GAP = 5
 gcounter = Counter()
 
 HEADERS = {
@@ -23,36 +26,70 @@ HEADERS = {
 }
 
 
+def load_page_no():
+    persistent_page_no_file = 'persistent_page_no_file.txt'
+    if not os.path.exists(persistent_page_no_file):
+        return 0
+    with open(persistent_page_no_file) as fd:
+        page = fd.read().strip()
+        if page == '':
+            return 0
+    return int(page)
+
+
+def write_page_no(pageno):
+    persistent_page_no_file = 'persistent_page_no_file.txt'
+    with open(persistent_page_no_file, 'w') as fd:
+        fd.write(str(pageno))
+
+
 def make_session():
     session = requests.Session()
     session.mount('http://', requests.adapters.HTTPAdapter(pool_connections=30, pool_maxsize=30, max_retries=3))
     session.headers = HEADERS
     return session
 
-def get_gonggao():
+
+def get_gonggao(pageno):
     session = make_session()
+    tail = 35349
+    notice_urls = 'notice_urls.txt'
+    head = pageno + 1
 
     refer = None
-    content_urls = []
-    for i in xrange(1, 35349):
-        url = 'http://data.eastmoney.com/Notice/Noticelist.aspx?type=0&market=all&date=&page={}'.format(i)
-        if refer is None:
-            refer = url
-        session.headers.update({'Referer': refer})
-        try:
-            resp = session.get(url)
-            if resp.status_code != 200 or resp.url != url:
+    fd = open('error_page.txt', 'a')
+    with open(notice_urls, 'a') as urlfd:
+        for i in xrange(head, tail):
+            url = 'http://data.eastmoney.com/Notice/Noticelist.aspx?type=0&market=all&date=&page={}'.format(i)
+            if refer is None:
+                refer = url
+            session.headers.update({'Referer': refer})
+            try:
+                for _ in range(3):
+                    resp = session.get(url)
+                    time.sleep(GAP)
+                    if resp.status_code != 200 or resp.url != url:
+                        continue
+                    tree = lxml.html.fromstring( resp.content.decode('gb18030') )
+                    urls = tree.xpath('//td[@class="title"]/a/@href')
+                    if urls == []:
+                        continue
+                    else:
+                        break
+                else:
+                    gcounter['error'] += 1
+                    fd.write(url)
+                    fd.write('\n')
+                for url in urls:
+                    urlfd.write( urlparse.urljoin('http://data.eastmoney.com/', url) )
+                    urlfd.write('\n')
+
+            except Exception as e:
                 gcounter['error'] += 1
-                continue
-            tree = lxml.html.fromstring( r.content.decode('gb18030') )
-            urls = tree.xpath('//td[@class="title"]/a/@href')
-            if urls == []:
-                gcounter['error'] += 1
-            for url in urls:
-                content_urls.append( urlparse.urljoin('http://data.eastmoney.com/', url) )
-
-        except Exception as e:
-            gcounter['error'] += 1
-            print(e)
+                print(e)
+            write_page_no(i)
+    fd.close()
 
 
+pageno = load_page_no()
+get_gonggao(pageno)
