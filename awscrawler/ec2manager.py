7c72b051292ec5468871581be663c2872d7e5318
yuande <miraclecome@gmail.com>
2016-06-21 21:02:07 +0800

    add new job

--- awscrawler/ec2manager.py
+++ awscrawler/ec2manager.py
@@ -19,7 +19,7 @@ if REGION_NAME == 'us-west-1':
 elif REGION_NAME == 'ap-northeast-1':
     KEYPAIR = 'crawl-tokyo'
     SECURITYGROUPID = 'sg-fcbf0998'
-    AMI_ID = 'ami-c337c3a2'
+    AMI_ID = 'ami-b825d1d9'
 
 INSTANCE_TYPE = 't2.nano'
 
