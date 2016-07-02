c4a874c26082d45fc180f400b321613ce95174a6
yuande <miraclecome@gmail.com>
2016-07-02 15:37:54 +0800

    merge baike files

---
+++ projects/baikekg/merge.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+from __future__ import print_function, division
+
+import hashlib
+import urllib
+import json
+import os
+from collections import defaultdict, Counter
+
+def merge_fudankg(bucketname):
+    """ TODO need generate url from entities file.
+        or the old style url will cause lack of tags and info.
+    """
+    batch_id = 'fudankg-json-20160625'
+    data = defaultdict(dict)
+    entities = set()
+    gcount = Counter()
+
+    def find_other_package(entity, original):
+        if original not in [u'av path', u'Information', u'Tags']:
+            return
+
+        ent = urllib.quote(entity.encode('utf-8'))
+        urlpattern = {
+            u'av pair': 'https://crl.ptopenlab.com:8800/cndbpedia/api/entityAVP?entity={}',
+            u'Information': 'https://crl.ptopenlab.com:8800/cndbpedia/api/entityInformation?entity={}',
+            u'Tags': 'https://crl.ptopenlab.com:8800/cndbpedia/api/entityTag?entity={}',
+        }
+
+        for k, v in urlapttern.iteritems():
+            gcounter[original] += 1
+            if k == original:
+                continue
+
+            url = v.format(ent)
+            url_hash = hashlib.sha1(url).hexdigest()
+            filename = '{}_{}'.format(batch_id, url_hash)
+            fname = os.path.join(bucketname, filename[-1], filename)
+            with open(fname) as fd:
+                js = json.load(fd)
+            _, v = js.items()[0]
+            data[entity][original] = v[original]
+
+
+
+    for fdir in os.listdir(bucketname):
+        fdir = os.path.join(bucketname, fdir)
+        for f in os.listdir(fdir):
+            fname = os.path.join(fdir, f)
+            with open(fname) as fd:
+                js = json.loads(fd.read())
+            for entity, v in js.items():
+                if entity in entities:
+                    continue
+
+            if u'Tags' in v:
+                if v[u'Tags'] != []:
+                    data[entity][u'Tags'] = v[u'Tags']
+                find_other_package(entity, u'Tags')
+                entities.add(entity)
+            elif u'Information' in v:
+                if v[u'Information'] != u'' and v[u'Information'] != []:
+                    data[entity][u'Information'] = v[u'Information']
+                find_other_package(entity, u'Information')
+                entities.add(entity)
+            elif u'av pair' in v:
+                if v[u'av pair'] != []:
+                    data[entity][u'av pair'] = v[u'av pair']
+                find_other_package(entity, u'av pair')
+                entities.add(entity)
+
+            gcounter['count'] += 1
+            if len(data) > 10000:
+                with open(gcounter['count'], 'w') as fd:
+                    json.dump(data, fd)
+                data = defaultdict(dict)
+
+    if len(data) >= 1:
+        with open(gcounter['count'], 'w') as fd:
+            json.dump(data, fd)
