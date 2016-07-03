f06399cdf45482937f807f8c21d00d3ff4574aec
yuande <miraclecome@gmail.com>
2016-07-04 00:06:12 +0800

    add reducer

---
+++ haizhicommon/reducer/action.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+from __future__ import print_function, division
+
+class Action(object):
+    def __init__(self):
+        pass
+
---
+++ haizhicommon/reducer/fetcher.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+from __future__ import print_function, division
+
+class Fetcher(object):
+    def __init__(self):
+        pass
+
+    def fetch(self):
+        pass
