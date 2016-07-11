53eae0d13318eb4e1295b9e959f2d753ca2b2dab
johnson <johnson@MacBook-Pro.local>
2016-07-11 15:57:28 +0800

    some changes in format

--- projects/mingluji/violence.py
+++ projects/mingluji/violence.py
@@ -7,6 +7,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+
 class MingluSpider(object):
 
     def __init__(self):
@@ -20,32 +21,28 @@ class MingluSpider(object):
         record = int(record)
         self.record = record
 
-
     def update_record(self):
-        self.record+=1
-        if self.record % 100==0:
+        self.record += 1
+        if self.record % 100 == 0:
             print self.record
         f = open('persistent_outtxt_lineno.txt', 'w')
         f.write(str(self.record))
         f.close()
 
-
     def GetPosition(self):
         for _ in range(self.record):
             self.url_file.readline()
 
-
-    def get_name_with_re(self,content):
-        m=re.findall('<span class="field-content"><a href="(.*?)">(.*?)</a></span>',content)
-        res=[i[1] for i in m]
+    def get_name_with_re(self, content):
+        m = re.findall(
+            '<span class="field-content"><a href="(.*?)">(.*?)</a></span>', content)
+        res = [i[1] for i in m]
         return res
 
-
-    def get_name(self,content):
-        dom=lxml.html.fromstring(content)
-        name=dom.xpath('//span[@class="field-content"]//text()')
+    def get_name_with_xpath(self, content):
+        dom = lxml.html.fromstring(content)
+        name = dom.xpath('//span[@class="field-content"]//text()')
         return name
-
 
     def traversal(self):
         self.GetPosition()
@@ -54,23 +51,23 @@ class MingluSpider(object):
             if not url:
                 break
             try:
-                content=requests.get(url).text
+                content = requests.get(url).text
             except:
-                content=requests.get(url).text
+                content = requests.get(url).text
                 print 'error in getting content with {}'.format(url)
                 self.update_record()
                 continue
-            name=self.get_name(content)
-            if not name :
+            name = self.get_name(content)
+            if not name:
                 if 'block-gongshang-mingluji-com-province-industry' not in content:
                     print url
                     print 'ERROR PAGE!!'
                     return
                 continue
             for i in name:
-                self.out.write(i+'\n')
+                self.out.write(i + '\n')
             self.update_record()
 
 
-obj=MingluSpider()
+obj = MingluSpider()
 obj.traversal()
