09bc3a12e858797e235249b29fa59c1bd7a80fbf
yuande <miraclecome@gmail.com>
2016-07-26 13:18:02 +0800

    add

--- projects/devops/monitor.py
+++ projects/devops/monitor.py
@@ -23,7 +23,7 @@ def mongo_connection():
     if result > 4096:
         slack('{} server mongo conn: {}'.format(ENV, result))
 
-    return result
+    return ret
 
 def connection():
     cmd = 'netstat -antp | wc -l'
@@ -32,7 +32,7 @@ def connection():
     if result > 4096:
         slack('{} server conn: {}'.format(ENV, result))
 
-    return result
+    return ret
 
 def jsvc_thread():
     cmd = "pstree | grep jsvc | awk -F'-' {'print $NF'}"
@@ -40,7 +40,8 @@ def jsvc_thread():
     result = int( ret[:ret.find('*')] )
     if result > 2048:
         slack('{} server jsvc thread: {}'.format(ENV, result))
-    return result
+
+    return ret[:ret.find('*')]
 
 def jsvc_fd():
     cmd = "ls -al /proc/`ps aux | grep jsvc | grep -v grep | awk {'print $2'} | sort -r | head -n 1`/fd | wc -l"
@@ -49,7 +50,7 @@ def jsvc_fd():
     if result > 2048:
         slack('{} server jsvc fd: {}'.format(ENV, result))
 
-    return result
+    return ret
 
 def run(cmd):
     from subprocess import Popen, PIPE, STDOUT
@@ -80,13 +81,16 @@ def get_running_stat(url_temlate):
     return True
 
 def main():
-    while 1:
-        mongo_connection()
-        connection()
-        jsvc_thread()
-        jsvc_fd()
-        get_running_stat('http://api.ruyi.ai/v1/message?app_key=28b12c3d-fab6-4540-af27-460277aa1a58&user_id=123&q={}')
-        time.sleep(30)
+    with open('monitor.log', 'w') as fd:
+        while 1:
+            line = [datetime.now().isoformat()]
+            line.append( mongo_connection() )
+            line.append( connection() )
+            line.append( jsvc_thread() )
+            line.append( jsvc_fd() )
+            line.append( get_running_stat('http://api.ruyi.ai/v1/message?app_key=28b12c3d-fab6-4540-af27-460277aa1a58&user_id=123&q={}') )
+            fd.write( '\t'.join(line) + '\n' )
+            time.sleep(30)
 
 if __name__ == '__main__':
     main()
