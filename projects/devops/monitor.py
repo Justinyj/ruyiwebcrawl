f73644016c69b3aa498f694d3f2395fda971e58f
shengshuaijohnson <120559187@qq.com>
2016-07-26 19:42:45 +0800

    change monitor

--- projects/devops/monitor.py
+++ projects/devops/monitor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: Yuande Liu <miraclecome (at) gmail.com>
-import requests
-import random
 import string
+import random
+import requests
 import json
 import os
 import time
@@ -74,13 +74,11 @@ def get_running_stat(url_temlate):
         if response.status_code == 200:
             break
     else:
-        slack('{} server conn: failed'.format(ENV))
-        return -1
+        return False
     res = response.json()
     process_milliseconds = res['result']['meta_process_milliseconds']
 
-    if process_milliseconds > 500 :
-        slack('{} server high process_millisecons in 3 mins : {}'.format(ENV, process_milliseconds))
+
     return process_milliseconds
 
 def get_minimum_ms(url_temlate):
@@ -91,24 +89,39 @@ def get_minimum_ms(url_temlate):
             slack('{} server conn: failed'.format(ENV))
             return -1
         min_ms = min(min_ms, process_ms)
-        sleep(30)
     if min_ms > 500:
-        slack('{} server high process_millisecons in 3 mins : {}'.format(ENV, min_ms))
+        slack('{} server in high process_millisecons : {}'.format(ENV, min_ms))
     return min_ms
 
 
 
 def main():
-    with open('monitor.log', 'w') as fd:
-        while 1:
-            line = [datetime.now().isoformat()]
-            line.append( mongo_connection() )
-            line.append( connection() )
-            line.append( jsvc_thread() )
-            line.append( jsvc_fd() )
-            line.append( str(get_running_stat('http://api.ruyi.ai/v1/message?app_key=28b12c3d-fab6-4540-af27-460277aa1a58&user_id=123&q={}') ))
-            fd.write( '\t'.join(line) + '\n' )
-            time.sleep(30)
+    counter = 0
+    min_ms = 999
+    while 1:
+        line = [datetime.now().isoformat()]
+        line.append( mongo_connection() )
+        line.append( connection() )
+        line.append( jsvc_thread() )
+        line.append( jsvc_fd() )
+
+        process_ms = get_running_stat('http://api.ruyi.ai/v1/message?app_key=28b12c3d-fab6-4540-af27-460277aa1a58&user_id=123&q={}')
+        line.append( str(process_ms) )
+        print line
+        fd = open('monitor.log', 'a')
+        fd.write( '\t'.join(line) + '\n' )
+        fd.close()
+
+        min_ms = min(min_ms, process_ms)
+        counter += 1
+        if counter == 6:
+            if min_ms >  500:
+                slack('{} server high process_millisecons in 3 mins : {}'.format(ENV, min_ms))
+                print 'send a message'
+            counter = 0
+            min_ms = 999
+
+        time.sleep(30)
 
 if __name__ == '__main__':
     main()
