299627dd513055c054506bfa5ff622da0b73d071
YingqiCol <yw2810@columbia.edu>
2016-07-31 22:53:37 -0500

    Update prefetch_tool.py

--- awscrawler/invoker/prefetch_tool.py
+++ awscrawler/invoker/prefetch_tool.py
@@ -50,7 +50,6 @@ def run(config):
 
     url_pattern = job["url_pattern"] if "url_pattern" in job else None
     urls_func = partial(load_urls, job["filename_urls"], url_pattern)
-    
     if config.get("debug"):
         slack( u"run {} batch_id: {}, urls length: {} debug: {}".format(
             config["note"],
