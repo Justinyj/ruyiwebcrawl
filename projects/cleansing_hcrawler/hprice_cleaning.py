66bbe50e88fb89a814c9b22b9ba0231b45b997f5
shengshuaijohnson <120559187@qq.com>
2016-08-16 17:05:38 +0800

    update

--- projects/cleansing_hcrawler/hprice_cleaning.py
+++ projects/cleansing_hcrawler/hprice_cleaning.py
@@ -38,44 +38,7 @@ class HpriceCleansing(object):
 
 
     def parse_single_item(self, item):
-<<<<<<< HEAD
-            # 时间关系，这里暂时设成父类方法为分析药通网，未来可以调整优化
-            # 药通网的price_history是一个字典，key为日期，value为价格
-            item_suit_schema = {
-                        'name' : item[u'name'],                         # 品种
-                        'productGrade' : '',                            # 产品等级
-                        'price' : item[u'price_history'],               # 价格历史
-                        'priceCurrency' : 'CNY',                        # 价格货币，命名规则使用iso-4217
-                        'productPlaceOfOrigin' : item[u'info'][u'产地'],  # 原产地
-                        'maxPrice' : '',                                # 最高价
-                        'seller' : '',                                  # 销售
-                        'source' : item[u'source'],                      # 数据源url
-                        'tags' : '',                                    # 标签   
-                        'productionYear' : '',                          # 生产年限
-                        'unitText' : item[u'info'][u'规格'],              # 单位，规格
-                        'mainEntityOfPage' : '',                        # 
-                        'sellerMarket' : '',                            # 报送单位(在中华粮网里出现，是各地市场)
-                        'minPrice' : '',                                # 最低价
-                        'productSpecification' : '',                    # 产品说明
-                        'priceType' : '',                               # 价格类型
-                        'description' : '',                             # 产品描述
-            }
-            
-            for k,v in item[u'price_history'].iteritems():
-
-
-                item_suit_schema['validDate'] = k   #日期
-                item_suit_schema['price']     = v   #价格
-                item_suit_schema['id']  = item_suit_schema['name'] + k
-                self.jsons.append(item_suit_schema)
-                self.counter += 1
-                if self.counter >= 2000:
-                    sendto_es(self.jsons)
-                    self.counter = 0
-                    self.jsons = []
-=======
         pass
->>>>>>> bd056bf95dafca08eb1d8a03b90840521b438b34
 
     def run(self):
         self.set_source_files_path()
