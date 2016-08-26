0dc5a7766dce918bc1029b1f5ef3dce5ed24cb47
Li Ding <lidingpku@gmail.com>
2016-08-26 11:38:34 +0800

    update file name kuwo

--- projects/kuwo/verification/local_songs.py
+++ projects/kuwo/verification/local_songs.py
@@ -8,7 +8,7 @@
 import json
 from collections import defaultdict
 
-fsong = open("/home/wl/kuwoFile1/m.json","r")
+fsong = open("/home/wl/kuwoFile1/musicFile.json","r")
 fartist = open("/home/wl/kuwoFile1/artistFile.json")
 fdupa = open("./dup_artist.txt","w")   #dumplicate artist id
 fdupm = open("./dup_music.txt","w")    #dumplicate music id
@@ -21,7 +21,7 @@ mmusic = {}
 dup_music = []
 
 #重复的歌手id
-for line in fartist:   
+for line in fartist:
 	art = json.loads(line)
 	if artists.has_key(art['id']):
 		dup_artist.append(art['id'])
@@ -39,7 +39,7 @@ for line in fsong:
 
 	if mmusic.has_key(line):
 		dup_music.append(music['musicrid'] + "__" + music['artistid'])
-	mmusic[line] = 0	
+	mmusic[line] = 0
 
 # dup_music.sort()
 for d in dup_music:
@@ -55,6 +55,6 @@ fsum.write("total: " + str(total))
 
 '''
 重复出现一次的歌手数目 4000左右
-  
+
 重复写了一次的歌曲数目 16万左右 去重后剩余250万左右歌曲
 '''
