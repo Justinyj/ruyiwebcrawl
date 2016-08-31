79eb6693cbbe0bb42022317d0f8a15f1f1bb4a02
yuande <miraclecome@gmail.com>
2016-08-31 10:51:52 +0800

    add tags and alias

--- projects/hcleansing/loader.py
+++ projects/hcleansing/loader.py
@@ -4,6 +4,7 @@
 
 from __future__ import print_function, division
 
+import json
 import re
 
 from urlparse import urlparse
@@ -30,3 +31,26 @@ class Loader(object):
 
     def parser(self, jsn):
         pass
+
+    @staticmethod
+    def get_tags_alias(tags_datadir):
+        """ 只有把名字对应到实体，才有tags和alias两个字段可以加入
+        """
+        tags_dict = {}
+        alias_dict = {}
+        for f in os.listdir(tags_datadir):
+            fname = os.path.join(tags_datadir, f)
+            with open(fname) as fd:
+                for entity, dic in json.load(fd).iteritems():
+
+                    for label, value in dic.iteritems():
+                        if label == u'Tags':
+                            tags_dict[entity] = value
+                        elif label == u'av pair':
+                            for a, v in value:
+                                if a in [u'别名',u'又称',u'其他名称']:
+                                    if entity not in alias_dict:
+                                        alias_dict[entity] = [v]
+                                    else:
+                                        alias_dict[entity].append(v)
+        return tags_dict, alias_dict
