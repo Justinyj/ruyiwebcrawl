08bfc5ee5ff8de39c5f9280c0cfe431d0bb54846
shengshuaijohnson <120559187@qq.com>
2016-09-12 15:00:48 +0800

    update

--- projects/hcleansing/kmzydaily_loader.py
+++ projects/hcleansing/kmzydaily_loader.py
@@ -72,7 +72,6 @@ class KmzydailyLoader(Loader):
             except DuplicateKeyError as e:
                 print (e)
 
-
 if __name__ == '__main__':
     obj = KmzydailyLoader()
     obj.read_jsn('/tmp/kmzydaily-20160912')
