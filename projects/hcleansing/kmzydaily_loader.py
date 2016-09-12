03bca04e764852a41e71666975bbfedfd1e793ee
shengshuaijohnson <120559187@qq.com>
2016-09-12 12:03:10 +0800

    update

--- projects/hcleansing/kmzydaily_loader.py
+++ projects/hcleansing/kmzydaily_loader.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: Yixuan Zhao <johnsonqrr (at) gmail.com>
+# 数据说明：（总json为271*20=5420条左右）
+# 1.有5%左右的数据是重复的，即完全一样，经检验是网站方面的问题，导入时会有三百条报重复，属正常情况。
+# 2.在9月12号对数据中validDate分布的统计：  Counter({u'2016-09-11': 3708, u'2016-09-09': 909, u'2016-08-20': 792, u'2016-08-28': 2, u'2016-08-25': 1, u'2016-09-05': 1})
+
 
 from __future__ import print_function, division
 
@@ -15,32 +19,38 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+
 class KmzydailyLoader(Loader):
     def read_jsn(self, data_dir):
         for fname in os.listdir(data_dir):
             for js in libfile.read_file_iter(os.path.join(data_dir, fname), jsn=True):
                 self.parse(js)
     def parse(self, jsn):
-        domain = self.url2domain(jsn[u'source'])
-        # name = jsn[u'name'].encode('utf-8')
-        rows  = jsn[u'rows']
-        print (len(rows))
+        source = jsn[u'source']
+        domain = self.url2domain(source)
 
-        print(name)
         for row in jsn[u'rows']:
-            trackingId = hashlib.sha1('{}_{}'.format(jsn[u'source'], jsn[u'access_time'])).hexdigest()
-            
+            trackingId = hashlib.sha1('{}_{}'.format(source, row[u'access_time'])).hexdigest()
+            name = row[u'drug_name']
+            priceType = ''
+            productPlaceOfOrigin = row[u'origin']
+            sellerMarket = row[u'site']
+            productGrade = row[u'standards']
+            validDate = row[u'price_date']
+            price = row[u'price']
+
             tags = [name, priceType, productPlaceOfOrigin, sellerMarket, productGrade]
             rid = hashlib.sha1('{}_{}_{}'.format('_'.join(tags), validDate, domain)).hexdigest()
+
             record = {
                 'rid': rid,
                 'gid': rid, # 不可变
                 'series': '_'.join(tags),
                 'tags': [ tag for tag in tags if tag],
-                'createdTime': datetime.utcnow(),
-                'updatedTime': datetime.utcnow(),
+                'createdTime': datetime.utcnow().isoformat(),
+                'updatedTime': datetime.utcnow().isoformat(),
                 'source': {
-                    'url': jsn[u'source'],
+                    'url': source,
                     'domain': domain,
                     'trackingId': trackingId,
                     'confidence': '0.6', 
@@ -48,20 +58,22 @@ class KmzydailyLoader(Loader):
                 'claims': [],
             }
 
+
+
             record['claims'].append({'p': u'productName', 'o': name})
             record['claims'].append({'p': u'validDate', 'o': validDate})
             record['claims'].append({'p': u'price', 'o': str(price)})
             record['claims'].append({'p': u'unitText', 'o': u'元/千克',})
             record['claims'].append({'p': u'productPlaceOfOrigin','o': productPlaceOfOrigin})
+            record['claims'].append({'p': u'sellerMarket','o': sellerMarket})
             record['claims'].append({'p': u'productGrade', 'o': productGrade})
             record['claims'].append({'p': u'priceCurrency', 'o': u'CNY' })
+            # print (json.dumps(record, ensure_ascii = False))
+            # return
             try:
                 self.node.insert(record)
             except DuplicateKeyError as e:
                 print (e)
-
-
-    pass
 
 
 if __name__ == '__main__':
