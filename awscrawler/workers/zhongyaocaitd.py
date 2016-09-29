95b892fcfff446955b20b5e9de126ac3a8b91480
shengshuaijohnson <120559187@qq.com>
2016-09-29 15:41:14 +0800

    update

--- awscrawler/workers/zhongyaocaitd.py
+++ awscrawler/workers/zhongyaocaitd.py
@@ -66,9 +66,9 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
                 data=data,
                 timeout=timeout,
                 refresh=True)
-
             if not content:
                 return False
+
             drug_list = json.loads(content)
             MBID_list = [ str(drug[u'MBID'])for drug in drug_list[u'Data']]
             manager.put_urls_enqueue(batch_id, MBID_list) # 每个MBID为一个数字
@@ -109,21 +109,20 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
                     timeout=timeout,
                     refresh=True)
                 if not price_content:
-                    print 'not'
                     return False
 
-                spec_info = sub_drug['MSpec'].split(' ')        # MSpec一般情况示例： 大统 东北  OR 统 较广
-                productGrade =   spec_info[0]                                         
-                if len(spec_info)==2:                           # MSpec特殊情况示例： 统 
-                    productPlaceOfOrigin = spec_info[1]
-                else:
+                spec_info = sub_drug['MSpec'].split(' ')        
+                productGrade = spec_info[0]                                         
+                if len(spec_info)==2:                           
+                    productPlaceOfOrigin = spec_info[1]         # MSpec一般情况示例： 大统 东北  OR 统 较广
+                else:                                           # MSpec特殊情况示例： 统
                     productPlaceOfOrigin = ''
                 price_item = json.loads(price_content)[u'Data']
                 price_data = price_item[u'PriceChartData']      # 注意price_data是一个字符串，需要再次loads后变为一个列表，每个列表代表一个药市，其中还嵌套着价格列表,价格里时间表示为时间戳等。
                                                                 # 由于后续处理较复杂，决定将这部分留到清洗脚本中完成，爬虫层面避免过多的清洗操作。
+
                 if price_data == '[]':                          # 即使存在这种规格，也有可能会没有价格历史
                     return True
-
                 result_item ={
                     'name' : sub_drug['MName'],
                     'productGrade': productGrade,
