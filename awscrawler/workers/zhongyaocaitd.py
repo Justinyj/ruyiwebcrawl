c2cd85343425862400d4aa1a4e5d51fce0d20da0
shengshuaijohnson <120559187@qq.com>
2016-09-29 15:26:53 +0800

    update

--- awscrawler/workers/zhongyaocaitd.py
+++ awscrawler/workers/zhongyaocaitd.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: Yixuan Zhao <johnsonqrr (at) gmail.com>
+# 爬取逻辑：
+# 1.先通过首字母得到各个药材的MBID，如刀豆的MBID是133
+# 2.再通过http://www.zyctd.com/Breeds/GetMCodexPoolListByMBID这个URL得到每个MBID下的各种规格-产地，和相应的MBSID
+# 如post刀豆的MBID=133后返回一个列表：红统-较广 MBSID=133301、白统-较广 MBDIS=13302
+# 3.最后通过这个MBSID，再对一个URL进行query，可以获得价格历史，价格历史中会分不同市场
 
 import json
 import urllib
@@ -13,7 +18,7 @@ from datetime import datetime
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-sys.path.append('..')
+
 from downloader.cacheperiod import CachePeriod
 from downloader.downloader_wrapper import Downloader
 from downloader.downloader_wrapper import DownloadWrapper
@@ -50,11 +55,10 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
         if label == 'first_letter':
             first_letter = url
             data = {
-                'Data':'{{\"url\": \"\", \"letter\": \"{}\"}}'.format(first_letter)
-            }  # form data示例： Data:{"letter":"J","url":""} 
-            
-            query_url = 'http://yaocai.zyctd.com/Ajax/AjaxHandle.ashx?CommandName=common/MCodexService/GetCodexNameByLetter'
+                'Data':'{{\"url\": \"\", \"letter\": \"{}\"}}'.format(first_letter)  # form data示例： Data:{"letter":"J","url":""} 
+            }      
 
+            query_url = 'http://yaocai.zyctd.com/Ajax/AjaxHandle.ashx?CommandName=common/MCodexService/GetCodexNameByLetter'
             content = process._downloader.downloader_wrapper(query_url,
                 batch_id,
                 gap,
@@ -83,18 +87,15 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
                 data=data,
                 timeout=timeout,
                 refresh=True)
-
             if not content:
                 return False
+
             item = json.loads(content)
             sub_drug_list = item[u'Data']
-            if not sub_drug_list:           # 请求成功然而列表为空，说明这种药材没有价格数据
+            if not sub_drug_list:           # 请求成功然而列表为空，说明这种药材没有价格数据，属正常情况
                 return True
 
             for sub_drug in sub_drug_list:    # eg：一个刀豆list里根据不同规格和产地的刀豆会分为不同的sub_drug,拥有不同的MBSID
-                print sub_drug['MSpec'].encode('utf-8')
-                # print json.dumps(result_item, ensure_ascii=False)
-
                 price_history_url = 'http://www.zyctd.com/Breeds/GetPriceTrend'
                 data = {
                     'MBSID'   : sub_drug['MBSID'],
@@ -111,16 +112,16 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
                     print 'not'
                     return False
 
-                spec_info = sub_drug['MSpec'].split(' ')   # MSpec一般情况示例： 大统 东北
+                spec_info = sub_drug['MSpec'].split(' ')        # MSpec一般情况示例： 大统 东北  OR 统 较广
                 productGrade =   spec_info[0]                                         
-                if len(spec_info)==2:                      # MSpec特殊情况示例： 统 
+                if len(spec_info)==2:                           # MSpec特殊情况示例： 统 
                     productPlaceOfOrigin = spec_info[1]
                 else:
                     productPlaceOfOrigin = ''
                 price_item = json.loads(price_content)[u'Data']
-                price_data = price_item[u'PriceChartData']  # price_data是一串字符串，需要再次loads后变为一个列表，每个列表代表一个药市，其中还嵌套着价格列表,价格里时间表示为时间戳等。
-                                                            # 由于后续处理较复杂，决定将这部分留到清洗脚本中完成，爬虫层面避免过多的清洗操作。
-                if price_data == '[]':   # 即使存在这种规格，也有可能没有价格历史
+                price_data = price_item[u'PriceChartData']      # 注意price_data是一个字符串，需要再次loads后变为一个列表，每个列表代表一个药市，其中还嵌套着价格列表,价格里时间表示为时间戳等。
+                                                                # 由于后续处理较复杂，决定将这部分留到清洗脚本中完成，爬虫层面避免过多的清洗操作。
+                if price_data == '[]':                          # 即使存在这种规格，也有可能会没有价格历史
                     return True
 
                 result_item ={
@@ -132,14 +133,6 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
                     'price_data'    : price_data
                 }
 
-
-                process._cache.post(url, json.dumps(result_item, ensure_ascii = False), refresh = True)
-                # print json.dumps(result_item, ensure_ascii = False)
+                if not process._cache.post(url, json.dumps(result_item, ensure_ascii = False), refresh = True):
+                    return False
             return True
-
-
-
-
-    return 0
-if __name__ == '__main__':
-    process('1490', 'zhongyaocai-20160929','123','123',0)
