04ee4fb19dcef1586349894e8b9ebc472f355b4c
Yingqi <yw2810@columbia.edu>
2016-09-30 10:15:30 +0800

    qichacha final

--- awscrawler/workers/qichacha.py
+++ awscrawler/workers/qichacha.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 注意： 企查查不能与其他爬虫同时爬取，必须单独运行
-# 爬取逻辑：  1： 搜索公司名，将搜索结果即公司主页加入待爬取队列，将可能有的别名映射存入period cache
-#           2： 进入公司主页，将公司详情页面和对外投资页面加入队列
-#           3： 解析投资页面和详情页面，存入period cache
+# 爬取逻辑：  1： 搜索公司名，将搜索结果前十页的公司详情页及投资页加入待爬取队列，将别名映射存入period cache
+#           2： 解析投资页面和详情页面，若json内容不为空则存入period cache,否则丢弃
 
 from __future__ import print_function, division
 import sys
@@ -35,7 +34,6 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
         # cookie = "gr_user_id=0fceb70d-e0ab-4c16-8f21-d49b5d242b0e; PHPSESSID=ltro2cjbvonlg6mu4hupe7dcv1; CNZZDATA1254842228=371101890-1469690209-null%7C1472547698"
 
         if cookie:
-            # print("Got cookie:", cookie)
             headers.update({'Cookie': cookie})
         setattr(process, '_downloader', DownloadWrapper(None, headers))
     if not hasattr(process, '_cache'):
@@ -43,7 +41,7 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
 
     if not hasattr(process, '_regs'):
         setattr(process, '_regs', {
-            'search': re.compile(urlparse.urljoin(SITE, 'search\?key=(.+?)&index=(\d+)')),
+            'search': re.compile(urlparse.urljoin(SITE, 'search\?key=(.+?)&index=(\d+)&p=(\d+)')),
             'detail': re.compile(urlparse.urljoin(SITE, 'company_getinfos\?unique=(.+?)&companyname=(.+?)&tab=base')),
             'invest': re.compile(urlparse.urljoin(SITE, 'company_getinfos\?unique=(.+?)&companyname=(.+?)(?:&p=(\d+))?&tab=touzi(?:&box=touzi)?')),
         })
@@ -56,7 +54,8 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
     # if kwargs and kwargs.get("debug"):
     #     get_logger(batch_id, today_str, '/opt/service/log/').info('start download')
     def reformat(info):     # 将info按企查查页面顺序插入队列
-        temp = info['info'][:]
+        temp = info['info']
+        del info['info']
         info['info'] = []
         info['info'].append(("统一社会信用码", temp['unified_social_credit_code']))
         info['info'].append(("注册号", temp['registration_id']))
@@ -88,6 +87,20 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
             })
         return invest_dict
 
+    def parser_patch(tree, all_info):
+        shareholders = tree.xpath("//table[@class='table table-bordered']/tr")
+        for sh in shareholders[1:]:
+            name = sh.xpath('./td[1]/a[1]/text()')[0]
+            subscribe_money = sh.xpath("./td[2]/text()")[0]
+            actual_money = sh.xpath("./td[3]/text()")[0]
+            all_info['shareholders'].append({ 'name':name, 'subscribe_money': subscribe_money, 'actual_money': actual_money})
+        names = tree.xpath("//div[@class='clear']/a[@class='text-lg']/text()")
+        positions = tree.xpath("//div[@class='clear']/small/text()")
+
+        for name,pos in zip(names, positions):
+            all_info['executives'].append({'name': name, 'position': pos})
+        return all_info
+
 
     content = process._downloader.downloader_wrapper(url,
         batch_id,
@@ -95,15 +108,27 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
         method,
         timeout=timeout,
         encoding='utf-8')
-    # print(url)
-    # print(content)
+
+    cookie = kwargs.get('cookie', None)
+    if not cookie:
+        get_logger(batch_id, today_str, '/opt/service/log/').info("No cookie in worker")
+        return False
+
     if content == '':
         get_logger(batch_id, today_str, '/opt/service/log/').info("no content")
-        return False
+        content = requests.get(url, cookies={1: cookie}).text
+        if content:
+            get_logger(batch_id, today_str, '/opt/service/log/').info('got content')
+        if not content and url.endswith("tab=touzi&box=touzi"):
+            get_logger(batch_id, today_str, '/opt/service/log/').info("void invest page")
+            return True
+
+
 
 
     invest_pat = "http://www.qichacha.com/company_getinfos?unique={key_num}&companyname={name}&p={p}&tab=touzi&box=touzi"
     main_pat = "http://www.qichacha.com/company_getinfos?unique={key_num}&companyname={name}&tab=base"
+    search_pat = "http://www.qichacha.com/search?key={name}&index=0&p={p}"
     parser = QiParser()
     tree = lxml.html.fromstring(content.replace('<em>', '').replace('</em>', ''))
 
@@ -118,10 +143,13 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
 
         if label == 'search':   # 搜索页面解析
             comp_name = urllib.unquote(m.group(1))
-            
-            get_logger(batch_id, today_str, '/opt/service/log/').info('getting items', comp_name)
+            page = int(m.group(3))
             dic = { 'search_name': comp_name, 'names': [] }
             urls = []
+            pages = tree.xpath(".//a[@id=\"ajaxpage\"]/text()")
+            if '>' in pages and page < 10:
+                urls = [search_pat.format(name=comp_name, p=str(page + 1))]
+                manager.put_urls_enqueue(batch_id, urls)
             if tree.cssselect('.table-search-list') and tree.cssselect('.tp2_tit a'):
                 items = tree.cssselect('.table-search-list')
                 for idx, i in enumerate(items):
@@ -129,43 +157,36 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
                         continue
                     item = {}
                     item['name'] = i.xpath('.//*[@class=\"tp2_tit clear\"]/a/text()')[0]
-                    # get_logger(batch_id, today_str, '/opt/service/log/').info(item['name'])
+                    # print(item['name'], file=log_file)
                     item['href'] = i.xpath('.//*[@class=\"tp2_tit clear\"]/a/@href')[0]
                     item['status'] = i.xpath('.//*[@class=\"tp5 text-center\"]/a/span/text()')[0]
                     item['key_num'] = item['href'].split('firm_')[1].split('.shtml')[0]
-                    # get_logger(batch_id, today_str, '/opt/service/log/').info(item['key_num'])
-                    if idx == 0 and comp_name == item['name']:  # 若第一个搜索结果完全匹配则只添加第一个结果入待爬取队列
-                        get_logger(batch_id, today_str, '/opt/service/log/').info('appending', item['name'])
-                        urls.append(main_pat.format(key_num=item['key_num'], name=item['name']))
-                        urls.append(invest_pat.format(key_num=item['key_num'], name=item['name'], p='1'))
-                        break
-                    elif idx < 3:   # 如果第一个不完全匹配， 将前三个搜索结果加入待爬取队列
-                        urls.append(main_pat.format(key_num=item['key_num'], name=item['name']))
-                        urls.append(invest_pat.format(key_num=item['key_num'], name=item['name'], p='1'))
-                        dic['names'].append(item['name'])
+                    # print(item['key_num'], file=log_file)
+                    urls.append(main_pat.format(key_num=item['key_num'], name=item['name']))
+                    urls.append(invest_pat.format(key_num=item['key_num'], name=item['name'], p='1'))
+                    dic['names'].append(item['name'])
             if not urls:
-                get_logger(batch_id, today_str, '/opt/service/log/').info("no result")
                 return True
-            get_logger(batch_id, today_str, '/opt/service/log/').info('URLS:',str(urls))
             manager.put_urls_enqueue(batch_id, urls)
             if not dic['names']:
                 return True
             else:   # 不完全匹配时将search_name与前三个搜索结果存入json用作别名映射
                 data = json.dumps(dic, encoding='utf-8', ensure_ascii=False)
-                get_logger(batch_id, today_str, '/opt/service/log/').info('projection:', data)
                 return process._cache.post(url, data)
 
 
         elif label == 'detail':     # 解析详情页面
             comp_name = urllib.unquote(m.group(2))
-            get_logger(batch_id, today_str, '/opt/service/log/').info(comp_name, "detail page")
             all_info = parser.parse_detail(tree)
             all_info['name'] = comp_name
             all_info['source'] = url
             all_info['access_time'] = datetime.utcnow().isoformat()
+            all_info = parser_patch(tree, all_info)
             all_info = reformat(all_info)
             data = json.dumps(all_info, encoding='utf-8', ensure_ascii=False)
             get_logger(batch_id, today_str, '/opt/service/log/').info(data)
+            if not any([ i[1] for i in all_info['info']]):
+                return False
             return process._cache.post(url, data)
 
         else:           # 解析投资页面
@@ -176,7 +197,6 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
             if '>' in pages:
                 urls = [invest_pat.format(key_num=key_num, name=comp_name, p=str(page + 1))]
                 manager.put_urls_enqueue(batch_id, urls)
-            get_logger(batch_id, today_str, '/opt/service/log/').info(comp_name, "invest page")
             invest_dict = parse_company_investment(tree)
             if not invest_dict['sub_companies']:
                 return True
@@ -184,7 +204,6 @@ def process(url, batch_id, parameter, manager, other_batch_process_time, *args, 
             invest_dict['source'] = url
             invest_dict['access_time'] = datetime.utcnow().isoformat()
             data = json.dumps(invest_dict, encoding='utf-8', ensure_ascii=False)
-            get_logger(batch_id, today_str, '/opt/service/log/').info(data)
             return process._cache.post(url, data)
 
 
